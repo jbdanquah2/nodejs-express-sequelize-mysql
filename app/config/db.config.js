4e10b40f4840452e868d415fe728e5db58baa3b9
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2020-10-06 20:17:56 +0000

    add heroku cleardb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "soababio",
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b2114a871c2fd1",
+    PASSWORD: "a960e0be",
+    DB: "heroku_cf7b0ffad698692",
     dialect: "mysql",
     pool: {
       max: 5,
@@ -11,3 +11,5 @@ module.exports = {
       idle: 10000
     } 
   };
+
+  mysql://b2114a871c2fd1:a960e0be@us-cdbr-east-02.cleardb.com/heroku_cf7b0ffad698692?reconnect=true
